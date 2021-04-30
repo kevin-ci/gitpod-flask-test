93fe7a7217d823ef45ba8d4bec41e4ffe25f168d
kevin-ci <kevin@codeinstitute.net>
2021-04-30 14:07:39 +0000

    recreate bug with redirect

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main():
+    return redirect(url_for('hello'))
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
+
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host="0.0.0.0",
+        port=5000,
         debug=True)
